9f08d9e6f2e32e49d89c47fc76c820aea35faa28
Harpreet Singh <harry.singh1064@gmail.com>
2018-12-04 01:06:45 +0800

    Initial project setup.

---
+++ LWWElementSet/LWWElementSet.h
@@ -0,0 +1,18 @@
+//
+//  LWWElementSet.h
+//  LWWElementSet
+//
+//  Copyright © 2018 harry. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for LWWElementSet.
+FOUNDATION_EXPORT double LWWElementSetVersionNumber;
+
+//! Project version string for LWWElementSet.
+FOUNDATION_EXPORT const unsigned char LWWElementSetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LWWElementSet/PublicHeader.h>
+
+
